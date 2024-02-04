0b94673fb193155c61763f3f8503e42c9c182c39
Samarth <samarthrn19@gmail.com>
2024-02-04 19:11:19 +0530

    Setup of SMTP using nodemailer done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/server-config');
+const { sendBasicEmail } = require('./services/email-service');
 
 const app = express();
 
@@ -11,4 +12,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 // Listen on port 
 app.listen(PORT, () =>{
     console.log(`Server started running on port ${PORT}`);
+
+    sendBasicEmail(
+        'support@admin.com',
+        'samarthsa24@gmail.com',
+        'Project update - Samarth',
+        'Hey, This is a testing mail from the notification service of Samarth\'s project, the email notification has been setup successfully. More update will be shared in tonight\'s call with Samarth'
+    );
 });
